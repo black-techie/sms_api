9d856cf3fa6530a1623bbb3081883cbf73a6bfa5
black-techie <willbroadnyirenda@gmail.com>
2024-06-25 09:39:45 +0300

    added database

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 from flask import Flask, request, jsonify
 import uuid
 import os
+from pysondb import db
 
 
 app = Flask(__name__)
 
-sms = []
 
 @app.route("/android", methods=["GET", "POST"])
 def android():
-    global sms
+    a=db.getDb("./json.json")
+    sms = a.getAll()
     if request.method == "GET":
         return {"medic-gateway": True}, 200
     
     if request.method == "POST":
+        a.deleteAll()
         if len(sms):
-            sms_cpy = sms
-            sms =[]
-            return jsonify({"messages": sms_cpy}), 200
+            return jsonify({"messages": sms}), 200
 
 @app.route("/sms", methods = ["GET", "POST"])
 def sms():
-    global sms
+    a=db.getDb("./json.json")
     if request.method == "GET":
-        sms.append({
+        a.add({
 			"id": str(uuid.uuid4()),
 			"to": "0789105606",
 			"content": "testing"
         })
 
     if request.method == "POST":
-        sms.append({
+        a.add({
 			"id": str(uuid.uuid4()),
 			"to": "0789105606",
 			"content": "testing"
